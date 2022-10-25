86b0668c89186ea58ca1b1fd4ad6914f6dc33da2
José Aricochea Rios <72884590+josearicochearios@users.noreply.github.com>
2022-10-25 00:50:40 -0500

    V 2.7: Nose que es

--- src/test/java/com/vallegrande/edu/ms1_user/Ms1UserApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.vallegrande.edu.ms1_user;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Ms1UserApplicationTests {
-
-}
